2e8a7c797fc6214abffe223b2a8752803a8fae0e
HaninHaidrah <haneenhadey@gmail.com>
2021-12-07 23:35:12 +0200

    lab28

--- src/compononts/Result/index.js
+++ src/compononts/Result/index.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./result.scss";
-import JSONPretty from 'react-json-pretty';
+// import ReactJson from "react-json-view";
 
-export default function Results(data,count, header) {
+// export default function Results(data, header) {
+//   console.log(data);
+
+//   return (
+//     <>
+//       {data.data ? (
+//         <section className="result">
+//           <ReactJson
+//             src={header}
+//             name="Headers"
+//             iconStyle={"triangle"}
+//             collapsed={false}
+//             enableClipboard={false}
+//             displayDataTypes={false}
+//           />
+
+//           <ReactJson
+//             src={data}
+//             name="Response"
+//             iconStyle={"triangle"}
+//             collapsed={false}
+//             enableClipboard={false}
+//             displayDataTypes={false}
+//           />
+//         </section>
+//       ) : (
+//         <h2>Loading..</h2>
+//       )}
+//     </>
+//   );
+// }
+
+import JSONPretty from 'react-json-pretty';
+import 'react-json-pretty/themes/monikai.css';
+import LoadingSpinner from "../loading/loading";
+
+function Results(data) {
   return (
-    <section className="result" data-testid="renderedData">
-      <pre>
-        {data ? <JSONPretty  count={count} header={header} data={data} ></JSONPretty> : <h2>Loading..</h2>}
-      </pre>
+    <section className="result">
+    <pre>{data.data ? <JSONPretty data={data} /> : <LoadingSpinner/>}</pre>
     </section>
   );
 }
+
+export default Results;
